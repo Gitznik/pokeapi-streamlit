0adb6b1b86092fb1744a21771067f1c37e1258b5
Gitznik <r.offner@gmx.de>
2023-07-24 21:39:07 +0200

    Add docs

--- pokeapi/__init__.py
+++ pokeapi/__init__.py
@@ -4,6 +4,9 @@ import requests
 import pandas as pd
 from streamlit.connections import ExperimentalBaseConnection
 from dataclasses import dataclass
+from datetime import timedelta
+
+ttl_duration = float | timedelta | str | None
 
 
 @dataclass
@@ -15,13 +18,39 @@ class ApiResponse:
 
 
 class PokeApiConnection(ExperimentalBaseConnection[requests.Session]):
+    """
+    Connection for the PokeAPI REST API.
+
+    Args:
+        **kwargs: The keyword arguments are passed through to initiating
+            a requests.Session object.
+
+    Attributes:
+        base_url (str): The base URL to the PokeAPI REST API.
+    """
+
     base_url = "https://pokeapi.co/api/v2"
 
     def _connect(self, **kwargs) -> requests.Session:
         _ = kwargs
         return requests.session()
 
-    def list_available_endpoints(self, ttl: int = 3600, **kwargs):
+    def list_available_endpoints(
+        self, ttl: ttl_duration = 3600, **kwargs
+    ) -> pd.DataFrame:
+        """Lists all available endpoints on the API
+
+        Args:
+            ttl (ttl_duration): The maximum time to keep an entry in the cache
+            **kwargs: These are passed through to the requests.get method
+
+        Raises:
+            requests.HTTPError: Any HTTP error occured
+
+        Returns:
+            endpoints (DataFrame): A DataFrame containing all endpoints and their URLs
+        """
+
         @st.cache_data(ttl=ttl)
         def _request(**kwargs) -> pd.DataFrame:
             session = self._instance
@@ -32,18 +61,39 @@ class PokeApiConnection(ExperimentalBaseConnection[requests.Session]):
         return _request(**kwargs)
 
     def list_available_resources(
-        self, endpoint: str, ttl: int = 3600, **kwargs
+        self, endpoint: str, limit: int = 20, ttl: int = 3600, **kwargs
     ) -> pd.DataFrame:
+        """Lists all available resources within an endpoint
+
+        Args:
+            endpoint (str): Which endpoint to execute the API call against
+            limit (int): Maximum number of resources per page
+            ttl (ttl_duration): The maximum time to keep an entry in the cache
+            **kwargs: These are passed through to the requests.get method
+
+        Raises:
+            requests.HTTPError: Any HTTP error occured
+
+        Returns:
+            endpoints (DataFrame): A DataFrame containing all resources and their
+                URLs.
+        """
+
         @st.cache_data(ttl=ttl)
         def _request(endpoint: str, **kwargs) -> pd.DataFrame:
             session = self._instance
             path = [self.base_url, endpoint]
             path = "/".join(path)
-            res = ApiResponse(**session.get(path, **kwargs).json())
+            params = {"limit": limit}
+            res = session.get(path, params=params, **kwargs)
+            res.raise_for_status()
+            res = ApiResponse(**res.json())
             marker = res.next
             results = [res.results]
             while marker:
-                res = ApiResponse(**session.get(marker, **kwargs).json())
+                res = session.get(path, params=params, **kwargs)
+                res.raise_for_status()
+                res = ApiResponse(**res.json())
                 marker = res.next
                 results.append(res.results)
 
@@ -52,9 +102,24 @@ class PokeApiConnection(ExperimentalBaseConnection[requests.Session]):
 
         return _request(endpoint, **kwargs)
 
-    def query(
+    def get_resource(
         self, endpoint: str, id: str | int, ttl: int = 3600, **kwargs
     ) -> pd.DataFrame:
+        """Gets all information about a specific resource in an endpoint
+
+        Args:
+            endpoint (str): Which endpoint to execute the API call against
+            id (str | int): Either the ID or the name of the resource
+            ttl (ttl_duration): The maximum time to keep an entry in the cache
+            **kwargs: These are passed through to the requests.get method
+
+        Raises:
+            requests.HTTPError: Any HTTP error occured
+
+        Returns:
+            endpoints (dict): All information about the resource
+        """
+
         @st.cache_data(ttl=ttl)
         def _request(endpoint: str, id: str | int, **kwargs) -> pd.DataFrame:
             session = self._instance
